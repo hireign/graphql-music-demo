b9f5e3d7470dcfe0789e80291cf7dd2e400c6a79
Hirenkumar Khant <hirreign@gmail.com>
2021-10-02 13:30:09 -0300

    Create ArtistRepository.java

---
+++ src/main/java/com/hireign/graphqlmusicdemo/repository/ArtistRepository.java
@@ -0,0 +1,9 @@
+package com.hireign.graphqlmusicdemo.repository;
+
+import com.hireign.graphqlmusicdemo.pojo.Artist;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends MongoRepository<Artist, String> {
+}
