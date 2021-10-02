f8fbdbed91460b54b1aa99de2e7b71bf6a42da88
Hirenkumar Khant <hirreign@gmail.com>
2021-10-02 16:43:10 -0300

    association b/w song and artist
    establishing relationship b/w songs and artists using artist name

--- src/main/java/com/hireign/graphqlmusicdemo/datafetcher/SongDataFetcher.java
+++ src/main/java/com/hireign/graphqlmusicdemo/datafetcher/SongDataFetcher.java
@@ -1,10 +1,12 @@
 package com.hireign.graphqlmusicdemo.datafetcher;
 
 import com.hireign.graphqlmusicdemo.pojo.Song;
+import com.hireign.graphqlmusicdemo.repository.ArtistRepository;
 import com.hireign.graphqlmusicdemo.repository.SongRepository;
 import graphql.schema.DataFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -14,9 +16,18 @@ public class SongDataFetcher {
     @Autowired
     private SongRepository songRepository;
 
+    @Autowired
+    private ArtistRepository artistRepository;
+
     public DataFetcher<List<Song>> findAllSongs() {
+        return dataFetchingEnvironment -> songRepository.findAll();
+    }
+
+    public DataFetcher findArtistByName() {
         return dataFetchingEnvironment -> {
-            return songRepository.findAll();
+            Song song = dataFetchingEnvironment.getSource();
+            String artistName = song.getArtistName();
+            return artistRepository.findByName(artistName);
         };
     }
 }
--- src/main/java/com/hireign/graphqlmusicdemo/repository/ArtistRepository.java
+++ src/main/java/com/hireign/graphqlmusicdemo/repository/ArtistRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistRepository extends MongoRepository<Artist, String> {
+    Artist findByName(String name);
 }
