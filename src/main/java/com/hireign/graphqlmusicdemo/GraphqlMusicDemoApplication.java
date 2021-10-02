2449ee954873823a76743ee45fb2409bc6983c19
Hirenkumar Khant <hirreign@gmail.com>
2021-10-02 12:24:23 -0300

    initial commit

---
+++ src/main/java/com/hireign/graphqlmusicdemo/GraphqlMusicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hireign.graphqlmusicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlMusicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlMusicDemoApplication.class, args);
+	}
+
+}
